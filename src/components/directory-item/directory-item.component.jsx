7c6e1a90a63880d0ec738861786a3e214204c0db
Mohammad Rafi <mohammad.rafi@tothenew.com>
2022-07-11 13:16:12 +0530

    Made cart icon and directory styled components

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import './directory-item.style.scss';
+import React from "react";
+import {
+  DirectoryItemContainer,
+  Body,
+  BackgroundImage,
+} from "./directory-item.style.jsx";
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
   return (
-    <div className="directory-item-container">
-      <div
-        className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      />
-      <div className="body">
+    <DirectoryItemContainer>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
-      </div>
-    </div>
-  )
-}
+      </Body>
+    </DirectoryItemContainer>
+  );
+};
 
-export default DirectoryItem
+export default DirectoryItem;
